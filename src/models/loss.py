0de04129c65bb1320a06269de4ebfdbbb1bd70b2
Bardia Rezaeimehr <bardia.rms@gmail.com>
2026-04-21 19:20:20 +0330

    Contrastive loss class instead of method

--- src/models/loss.py
+++ src/models/loss.py
@@ -1,28 +1,28 @@
 import torch
 import torch.nn.functional as F
 
+class ContrastiveLoss:
 
+    def __call__(self, logits_per_image: torch.Tensor,
+                        logits_per_text: torch.Tensor)-> torch.Tensor:
+        """
+        Compute the contrastive loss (image-to-text + text-to-image).
 
-
-def contrastive_loss(logits_per_image, logits_per_text):
-    """
-    Compute the contrastive loss (image-to-text + text-to-image).
-
-    Args:
-        logits_per_image: similarity scores between images and all texts
-        logits_per_text: similarity scores between texts and all images
-    
-    Returns:
-        loss: A scalar contrastive loss value
-    """
-    # Compute image-to-text loss
-    labels = torch.arange(logits_per_image.size(0)).to(logits_per_image.device)
-    image_to_text_loss = F.cross_entropy(logits_per_image, labels)
-    
-    # Compute text_to_image loss
-    text_to_image_loss = F.cross_entropy(logits_per_text, labels)
-    
-    # Average the two losses
-    loss = (image_to_text_loss + text_to_image_loss) / 2
-    
-    return loss
+        Args:
+            logits_per_image: similarity scores between images and all texts
+            logits_per_text: similarity scores between texts and all images
+        
+        Returns:
+            loss: A scalar contrastive loss value
+        """
+        # Compute image-to-text loss
+        labels = torch.arange(logits_per_image.size(0)).to(logits_per_image.device)
+        image_to_text_loss = F.cross_entropy(logits_per_image, labels)
+        
+        # Compute text_to_image loss
+        text_to_image_loss = F.cross_entropy(logits_per_text, labels)
+        
+        # Average the two losses
+        total_loss = (image_to_text_loss + text_to_image_loss) / 2
+        
+        return total_loss
